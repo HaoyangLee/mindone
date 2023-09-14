e9f224b2fadfe9cfc95ecfbac355f8f69f29ffdc
HaoyangLI <417493727@qq.com>
2023-09-14 09:22:23 +0800

    add script for downloading data from ecs server to local hard disk

---
+++ examples/videocomposer/vc/utils/webvid-10m_script/ecs2local.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+root=data2
+set=2M_train
+partid=4
+
+for tar_name in 22 24 26 27 28 31 34 35 37 42 46 47
+do
+        tar_path=/$root/webvid-10m/dataset/$set/part$partid/000$tar_name.tar
+        echo Downloading $tar_path ...
+        scp -r root@159.138.153.179:$tar_path /e/webvid/dataset/$set/part$partid/ && echo Successfully downloaded!
+done
