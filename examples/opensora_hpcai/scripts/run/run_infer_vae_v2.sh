c575632c24aeda3b63e54c5691a348aab1d124fe
HaoyangLI <417493727@qq.com>
2025-03-27 15:55:13 +0800

    add infer_vae_v2.py
    add infer_vae_v2.py
    
    init infer_vae_v2.py
    
    update infer_vae_v2.py
    
    add shell script
    
    update infer_vae_v2.py
    
    update infer_vae_v2.py
    
    add cond for infer_vae_v2.py
    
    update
    
    encoder -> encode
    
    save latent mean and std instead of latent itself
    
    add num_frames
    
    set vae_micro_batch_size to None by default
    
    rm vae_micro_batch_size, dl_return_all_frames, batch_size, cond
    
    add return_frame_data, micro_batch_size in ds_config, image_size -> video_size
    
    fix shape
    
    rm cond, load config from yaml
    
    return_dataset=False
    
    update
    
    updates

---
+++ examples/opensora_hpcai/scripts/run/run_infer_vae_v2.sh
@@ -0,0 +1,6 @@
+python scripts/v2.0/infer_vae_v2.py \
+--csv_path /path/to/video_caption.csv \
+--video_folder /path/to/video \
+--output_path /path/to/output_path \
+--vae_checkpoint /path/to/vae_checkpoint \
+--video_size 256 256 \
--- examples/opensora_hpcai/scripts/v2.0/infer_vae_v2.py
+++ examples/opensora_hpcai/scripts/v2.0/infer_vae_v2.py
@@ -4,21 +4,22 @@ import os
 import sys
 import time
 from pathlib import Path
+import yaml
 
 import numpy as np
 from tqdm import tqdm
 
 import mindspore as ms
-from mindspore import nn
+from mindspore import mint, nn
 from mindspore.communication.management import get_group_size, get_rank, init
 
 __dir__ = os.path.dirname(os.path.abspath(__file__))
-mindone_lib_path = os.path.abspath(os.path.join(__dir__, "../../../"))
+mindone_lib_path = os.path.abspath(os.path.join(__dir__, "../../../../"))
 sys.path.insert(0, mindone_lib_path)
-sys.path.insert(0, os.path.abspath(os.path.join(__dir__, "..")))
+sys.path.insert(0, os.path.abspath(os.path.join(__dir__, "../../")))
 
 from opensora.datasets.video_dataset import create_dataloader
-from opensora.models.vae.vae import SD_CONFIG, AutoencoderKL
+from opensora.models.hunyuan_vae import CausalVAE3D_HUNYUAN
 from opensora.utils.amp import auto_mixed_precision
 from opensora.utils.model_utils import str2bool  # _check_cfgs_in_parser
 
@@ -29,6 +30,25 @@ from mindone.utils.seed import set_random_seed
 logger = logging.getLogger(__name__)
 
 
+def save_output(output_folder: Path,
+                video_name: Path,
+                mean: np.array,
+                std: np.array,
+                ):
+    fn = video_name.with_suffix(".npz")
+    npz_fp = os.path.join(output_folder, fn)
+    if not os.path.exists(os.path.dirname(npz_fp)):
+        os.makedirs(os.path.dirname(npz_fp))
+    if os.path.exists(npz_fp):
+        if args.allow_overwrite:
+            logger.info(f"Overwritting {npz_fp}")
+    np.savez(
+        npz_fp,
+        latent_mean=mean.astype(np.float32),
+        latent_std=std.astype(np.float32),
+    )
+
+
 def init_env(
     mode: int = ms.GRAPH_MODE,
     seed: int = 42,
@@ -36,7 +56,6 @@ def init_env(
     max_device_memory: str = None,
     device_target: str = "Ascend",
     jit_level: str = "O0",
-    global_bf16: bool = False,
 ):
     """
     Initialize MindSpore environment.
@@ -95,8 +114,8 @@ def init_env(
             "please ensure the MindSpore version >= ms2.3_0615, and use GRAPH_MODE."
         )
 
-    if global_bf16:
-        ms.set_context(ascend_config={"precision_mode": "allow_mix_precision_bf16"})
+    # if global_bf16:
+    #     ms.set_context(ascend_config={"precision_mode": "allow_mix_precision_bf16"})
 
     return rank_id, device_num
 
@@ -104,32 +123,27 @@ def init_env(
 def main(args):
     set_logger(name="", output_dir="logs/infer_vae")
     rank_id, device_num = init_env(
-        args.mode, args.seed, args.use_parallel, device_target=args.device_target, global_bf16=args.global_bf16
+        args.mode, args.seed, args.use_parallel, device_target=args.device_target,
     )
     print(f"rank_id {rank_id}, device_num {device_num}")
-
-    if args.resize_by_max_value and args.batch_size != 1:
-        raise ValueError(
-            f"Batch size must be 1 when `resize_by_max_value=True`, but get `batch_size={args.batch_size}`."
-        )
 
     # build dataloader for large amount of captions
     ds_config = dict(
         csv_path=args.csv_path,
         video_folder=args.video_folder,
-        sample_size=args.image_size,
+        sample_size=args.video_size,
         sample_stride=args.frame_stride,
-        micro_batch_size=args.vae_micro_batch_size,
+        micro_batch_size=args.num_frames,
         video_column=args.video_column,
         caption_column=args.caption_column,
-        return_frame_data=args.dl_return_all_frames,
+        return_frame_data=True,
         resize_by_max_value=args.resize_by_max_value,
-        transform_name=args.transform_name,
+        transform_name=args.transform_name,  # TODO: check whether align with original repo
         filter_data=args.filter_data,
     )
-    dataloader, ds = create_dataloader(
+    dataloader, _ = create_dataloader(
         ds_config,
-        args.batch_size,
+        batch_size=1,
         ds_name="video",
         num_parallel_workers=16,
         max_rowsize=256,
@@ -137,55 +151,31 @@ def main(args):
         device_num=device_num,
         rank_id=rank_id,
         drop_remainder=False,
-        return_dataset=True,
+        return_dataset=False,
     )
     dataset_size = dataloader.get_dataset_size()
     logger.info(f"Num batches: {dataset_size}")
 
     # model initiate and weight loading
     logger.info("vae init")
-    VAE_Z_CH = SD_CONFIG["z_channels"]
-    vae = AutoencoderKL(
-        SD_CONFIG,
-        VAE_Z_CH,
-        ckpt_path=args.vae_checkpoint,
-    )
-    vae = vae.set_train(False)
+    with open(args.config, "r") as f:
+        config = yaml.load(f, Loader=yaml.FullLoader)
+    ae_config = config["ae"]
+    ae_config["dtype"] = args.vae_precision
+
+    model_ae = CausalVAE3D_HUNYUAN(**ae_config).set_train(False)
+    del model_ae.decoder
+    
     dtype_map = {"fp16": ms.float16, "bf16": ms.bfloat16}
-    if args.dtype in ["fp16", "bf16"]:
+    if args.vae_precision in ["fp16", "bf16"]:
         vae = auto_mixed_precision(
             vae,
             amp_level=args.amp_level,
-            dtype=dtype_map[args.dtype],
+            dtype=dtype_map[args.vae_precision],
             custom_fp32_cells=[nn.GroupNorm],
         )
 
     logger.info("Start VAE embedding...")
-
-    def save_output(video_name: Path, mean, std=None, fps=None, ori_size=None):
-        fn = video_name.with_suffix(".npz")
-        npz_fp = os.path.join(output_folder, fn)
-        if not os.path.exists(os.path.dirname(npz_fp)):
-            os.makedirs(os.path.dirname(npz_fp))
-        if os.path.exists(npz_fp):
-            if args.allow_overwrite:
-                logger.info(f"Overwritting {npz_fp}")
-        if args.save_distribution:
-            np.savez(
-                npz_fp,
-                latent_mean=mean.astype(np.float32),
-                latent_std=std.astype(np.float32),
-                fps=fps,
-                ori_size=ori_size,
-            )
-        else:
-            np.savez(
-                npz_fp,
-                latent_mean=video_latent_mean.astype(np.float32),
-                fps=fps,
-                ori_size=ori_size,
-            )
-        return npz_fp
 
     # infer
     if args.csv_path is not None:
@@ -200,71 +190,36 @@ def main(args):
         ds_iter = dataloader.create_dict_iterator(1, output_numpy=True)
         for step, data in tqdm(enumerate(ds_iter), total=dataset_size):
             start_time = time.time()
-            # caption = data["caption"]
 
-            if args.dl_return_all_frames:
-                frame_data = data["frame_data"]
-                num_videos = frame_data.shape[0]
-                fps = data["fps"][0]
-                ori_size = data["ori_size"][0]
-                assert args.batch_size == 1, "batch size > 1 is not supported due to dynamic frame numbers among videos"
-                for i in range(num_videos):
-                    abs_video_path = data["video_path"][i]
-                    video_path = Path(abs_video_path).relative_to(args.video_folder)
+            frame_data = data["frame_data"]
+            num_videos = frame_data.shape[0]
+            for i in range(num_videos):
+                abs_video_path = data["video_path"][i]
+                video_path = Path(abs_video_path).relative_to(args.video_folder)
+                fn = video_path.with_suffix(".npz")
+                npz_fp = os.path.join(output_folder, fn)
+                if os.path.exists(npz_fp) and not args.allow_overwrite:
+                    logger.info(f"{npz_fp} exists, skip vae encoding")
+                    continue
 
-                    fn = video_path.with_suffix(".npz")
-                    npz_fp = os.path.join(output_folder, fn)
-                    if os.path.exists(npz_fp) and not args.allow_overwrite:
-                        logger.info(f"{npz_fp} exists, skip vae encoding")
+                x = frame_data[i]
+                x = np.expand_dims(np.transpose(x, (1, 0, 2, 3)), axis=0)  # [f, c, h, w] -> [b, c, f, h, w], b must be 1
+
+                if x.shape[2] < args.num_frames:
+                    msg = f"Video {video_path} is too short! It has {x.shape[2]} frames, but expected >= {args.num_frames} frames."
+                    if args.drop_short_video:
+                        logger.warning(f"{msg} Drop it")
                         continue
+                    else:
+                        raise ValueError(f"{msg} Please set a smaller --num_frames or set --drop_short_video=True")
+                x = x[:, :, : args.num_frames, :, :]
 
-                    video_latent_mean = []
-                    video_latent_std = []
+                logger.info(f"The shape [b, c, f, h, w] of video for vae encoding: {x.shape}")
 
-                    x = frame_data[i]
-                    bs = args.vae_micro_batch_size
-                    for j in range(0, x.shape[0], bs):
-                        x_bs = x[j : min(j + bs, x.shape[0])]
-                        mean, std = ms.ops.stop_gradient(vae.encode_with_moments_output(ms.Tensor(x_bs, ms.float32)))
-                        video_latent_mean.append(mean.asnumpy())
-                        if args.save_distribution:
-                            video_latent_std.append(std.asnumpy())
-
-                    video_latent_mean = np.concatenate(video_latent_mean, axis=0)
-                    if args.save_distribution:
-                        video_latent_std = np.concatenate(video_latent_std, axis=0)
-
-                    save_output(video_path, video_latent_mean, video_latent_std, fps, ori_size)
-            else:
-                num_videos = data["video_path"].shape[0]
-                for i in range(num_videos):
-                    abs_video_path = data["video_path"][i]
-                    video_path = Path(abs_video_path).relative_to(args.video_folder)
-
-                    fn = video_path.with_suffix(".npz")
-                    npz_fp = os.path.join(output_folder, fn)
-                    if os.path.exists(npz_fp) and not args.allow_overwrite:
-                        logger.info(f"{npz_fp} exists, skip vae encoding")
-                        continue
-
-                    video_latent_mean = []
-                    video_latent_std = []
-                    fps, ori_size = None, None
-                    for x_bs, fps, ori_size in ds.get_video_frames_in_batch(
-                        abs_video_path, micro_batch_size=args.vae_micro_batch_size, sample_stride=args.frame_stride
-                    ):
-                        mean, std = ms.ops.stop_gradient(vae.encode_with_moments_output(ms.Tensor(x_bs, ms.float32)))
-                        video_latent_mean.append(mean.asnumpy())
-                        if args.save_distribution:
-                            video_latent_std.append(std.asnumpy())
-                        fps = fps
-                        ori_size = ori_size
-
-                    video_latent_mean = np.concatenate(video_latent_mean, axis=0)
-                    if args.save_distribution:
-                        video_latent_std = np.concatenate(video_latent_std, axis=0)
-
-                    save_output(video_path, video_latent_mean, video_latent_std, fps, ori_size)
+                x_0, posterior = ms.ops.stop_gradient(model_ae.encode(ms.Tensor(x, ms.float32), return_posterior=True))
+                video_latent_mean = posterior.mean.asnumpy()
+                video_latent_std = posterior.std.asnumpy()
+                save_output(output_folder, video_path, video_latent_mean, video_latent_std)
 
             end_time = time.time()
             logger.info(f"Time cost: {end_time-start_time:0.3f}s")
@@ -277,6 +232,12 @@ def main(args):
 def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument(
+        "-c",
+        "--config",
+        default="configs/opensora-v2-0/train/image.yaml",
+        help="Path to load a config yaml file, but only use the ae config in it.",
+    )
+    parser.add_argument(
         "--csv_path",
         default=None,
         type=str,
@@ -288,39 +249,32 @@ def parse_args():
         default=None,
         help="output dir to save the embeddings, if None, will treat the parent dir of csv_path as output_path.",
     )
-    parser.add_argument(
-        "--save_distribution",
-        default=True,
-        type=str2bool,
-        help="If True, will save mean and std representing vae latent distribution. \
-                Otherwise, will only save mean (save half storage but loss vae sampling diversity).",
-    )
     parser.add_argument("--video_column", default="video", type=str, help="name of column for videos saved in csv file")
     parser.add_argument(
         "--caption_column", default="caption", type=str, help="name of column for captions saved in csv file"
     )
     parser.add_argument("--video_folder", default="", type=str, help="root dir for the video data")
     parser.add_argument("--filter_data", default=False, type=str2bool, help="Filter non-existing videos.")
-    parser.add_argument("--image_size", nargs="+", default=[512, 512], type=int, help="image size")
-    parser.add_argument(
-        "--dl_return_all_frames",
-        default=True,
-        type=str2bool,
-        help="dataloder return all frames. If True, read all frames in a video then do vae encoding in micro_batch_size (faster but cost more CPU memory). \
-                If False, read a clip of frames in a video in micro_batch_size and do vae encoding iteratively. (slower but memory efficient",
-    )
+    parser.add_argument("--video_size", nargs="+", default=[256, 256], type=int, help="video size for vae encoder input")
     parser.add_argument(
         "--vae_checkpoint",
         type=str,
-        default="stabilityai/sd-vae-ft-ema",
+        default="hpcai-tech/Open-Sora-v2/hunyuan_vae.safetensors",
         help="VAE checkpoint file path which is used to load vae weight.",
     )
+    # parser.add_argument(
+    #     "--sd_scale_factor", type=float, default=0.18215, help="VAE scale factor of Stable Diffusion model."
+    # )
     parser.add_argument(
-        "--sd_scale_factor", type=float, default=0.18215, help="VAE scale factor of Stable Diffusion model."
+        "--vae_precision",
+        type=str,
+        default="fp32",
+        choices=["bf16", "fp16", "fp32"],
+        help="Precision mode for the VAE model: fp16, bf16, or fp32.",
     )
 
     parser.add_argument("--device_target", type=str, default="Ascend", help="Ascend or GPU")
-    parser.add_argument("--mode", type=int, default=0, help="Running in GRAPH_MODE(0) or PYNATIVE_MODE(1) (default=0)")
+    parser.add_argument("--mode", type=int, default=1, help="Running in GRAPH_MODE(0) or PYNATIVE_MODE(1) (default=0)")
     parser.add_argument("--use_parallel", default=False, type=str2bool, help="use parallel")
     parser.add_argument("--seed", type=int, default=4, help="Inference seed")
     parser.add_argument(
@@ -329,13 +283,13 @@ def parse_args():
         type=str2bool,
         help="whether to enable flash attention. Default is False",
     )
-    parser.add_argument(
-        "--dtype",
-        default="fp32",
-        type=str,
-        choices=["bf16", "fp16", "fp32"],
-        help="what data type to use for latte. Default is `fp32`, which corresponds to ms.float16",
-    )
+    # parser.add_argument(
+    #     "--dtype",
+    #     default="fp32",
+    #     type=str,
+    #     choices=["bf16", "fp16", "fp32"],
+    #     help="what data type to use for latte. Default is `fp32`, which corresponds to ms.float16",
+    # )
     parser.add_argument(
         "--precision_mode",
         default=None,
@@ -352,11 +306,17 @@ def parse_args():
                 the AR of target image size then resize, suitable for where target h != target w.",
     )
     parser.add_argument(
-        "--vae_micro_batch_size",
+        "--num_frames",
         type=int,
-        default=64,
-        help="If not None, split batch_size*num_frames into smaller ones for VAE encoding to reduce memory limitation",
+        default=129,
+        help="Crop the video to num_frames frames.",
     )
+    parser.add_argument(
+        "--drop_short_video",
+        type=str2bool,
+        default=False,
+        help="How to deal with the video whose frames < args.num_frames. True: drop them; False: raise error, set a smaller --num_frames",
+    )    
     parser.add_argument(
         "--use_recompute",
         default=False,
@@ -364,18 +324,11 @@ def parse_args():
         help="whether use recompute.",
     )
     parser.add_argument(
-        "--global_bf16",
-        default=False,
-        type=str2bool,
-        help="Experimental. If True, dtype will be overrided, operators will be computered in bf16 if they are supported by CANN",
-    )
-    parser.add_argument(
         "--allow_overwrite",
         type=str2bool,
         default=False,
         help="If True, allow to overwrite the existing npz file. If False, will skip vae encoding if the latent npz file is already existed",
     )
-    parser.add_argument("--batch_size", default=1, type=int, help="batch size")
     parser.add_argument("--resize_by_max_value", default=False, type=str2bool, help="resize the image by max instead.")
 
     __dir__ = os.path.dirname(os.path.abspath(__file__))
